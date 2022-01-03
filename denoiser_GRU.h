d0c53b94eab53a587c6d0c51a3653562230767c0
Manuele Rusci <manuele.rusci@unibo.it>
2022-01-03 15:51:32 +0100

    added correct defines to denoiser_gru.h

--- denoiser_GRU.h
+++ denoiser_GRU.h
@@ -13,8 +13,12 @@
 
 #define __PREFIX(x) denoiser_GRU ## x
 #include "denoiser_GRUKernels.h"
+#include "denoiser_GRUInfo.h"
 #define denoiser_L1_SIZE _denoiser_GRU_L1_Memory_SIZE
 #define denoiser_L2_SIZE _denoiser_GRU_L2_Memory_SIZE
+
+#define SCALE_IN denoiser_GRU_Input_1_OUT_SCALE
+#define SCALE_OUT denoiser_GRU_Output_1_OUT_SCALE
 
 extern AT_HYPERFLASH_FS_EXT_ADDR_TYPE __PREFIX(_L3_Flash);
 
