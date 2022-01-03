4b5d2f9ef4054414010f3d894ac6eda7347cd621
Manuele Rusci <manuele.rusci@unibo.it>
2022-01-03 15:50:36 +0100

    reworked test script to test accuracy: now support nntool and ne16

--- test_accuracy/test_GAP.py
+++ test_accuracy/test_GAP.py
@@ -10,6 +10,12 @@ import shutil
 from pesq import pesq
 from pystoi import stoi
 
+# nntool
+sys.path.insert(0, os.environ['NNTOOL_DIR'])
+from execution.graph_executer import GraphExecuter
+from execution.quantization_mode import QuantizationMode
+from interpreter.nntool_shell import NNToolShell
+from quantization.quantizer.new_quantizer import NewQuantizer
 
 def run_on_gap_gvsoc(input_file, output_file, compile=True, gru=False, 
                 quant_bfp16=False,  quant_int8=False, approx='' ):
@@ -39,7 +45,8 @@ def run_on_gap_gvsoc(input_file, output_file, compile=True, gru=False,
     os.system(run_command)
     return True
 
-def denoise_sample(input_file, output_file, samplerate, padding):
+def denoise_sample_on_gap_gvsoc(input_file, output_file, samplerate, padding = False, compile_GAP=True, 
+                    gru=False, quant_bfp16=False, quant_int8=False, approx=''):
 
     if os.path.isfile(output_file):
         os.remove(output_file)
@@ -50,7 +57,8 @@ def denoise_sample(input_file, output_file, samplerate, padding):
 
     file_name =  os.getcwd() + '/samples/test_py.wav'
     sf.write(file_name, data, samplerate)
-    run_on_gap_gvsoc(file_name, output_file)
+    run_on_gap_gvsoc(file_name, output_file, compile=compile_GAP, 
+                    gru=gru, quant_bfp16=quant_bfp16, quant_int8=quant_int8, approx=approx)
     shutil.copyfile('BUILD/GAP9_V2/GCC_RISCV_PULPOS/test_gap.wav', output_file)
     if not os.path.isfile(output_file):
         print("Error! not any output fiule produced")
@@ -59,9 +67,8 @@ def denoise_sample(input_file, output_file, samplerate, padding):
     return 0
 
 def test_on_gap(    dataset_path, output_file, samplerate, padding, 
-                    suffix_cleanfile, gru, quant_bfp16, quant_int8, approx ):
+                    suffix_cleanfile, gru, quant_bfp16, quant_int8, quant_ne16, nntool, approx  ):
     
-
     # set noisy and clean path
     noisy_path = dataset_path + '/noisy/'
     clean_path = dataset_path + '/clean/'
@@ -86,6 +93,79 @@ def test_on_gap(    dataset_path, output_file, samplerate, padding,
     # utils
     compile_GAP = True
 
+    # prepare nntool graph
+    if nntool:
+        model_name = 'model/denoiser_GRU.onnx' if gru else 'model/denoiser.onnx'
+        G = NNToolShell.get_graph_from_commands([
+            'open ' + model_name + ' --use_lut_sigmoid --use_lut_tanh',
+            'adjust',
+            'fusions --scale8',
+            'nodeoption GRU_74 RNN_STATES_AS_INPUTS 1',
+            'nodeoption GRU_136 RNN_STATES_AS_INPUTS 1',
+
+        ])
+
+        if quant_ne16 or quant_int8:
+
+            # quantized inference
+            nntool_quantizer = True
+
+            # change this with other script...
+            import pickle
+            fp = open('model/data_quant_gru.json', 'rb')
+            astats = pickle.load(fp)
+            fp.close()
+            
+            Opts = { 'float_type': 'float32', 'kernel_type': 'fastfloat', 'hwc': False, 
+                     'sq_bits': 8, 
+                     'weight_bits': 8, 
+                     'force_external_size': 16, # 8
+                     'narrow_weights': True, 
+                     'use_ne16': True, # False
+                     'narrow_state': True, 
+                     'quantized_dimension': 'channel', 
+                     'force_ne16': False, 
+                     'allow_asymmetric': False, 
+                     'force_input_size': 16, # 8 
+                     'force_output_size': 16, #8 
+                     'softmax_out_8bits': False, 
+                     'bits': 16, 
+                     'pow2_biases': 0 
+                    }
+            if quant_int8: 
+                Opts['force_external_size'] = 8
+                Opts['use_ne16'] = False
+                Opts['force_input_size'] = 8
+                Opts['force_output_size'] = 8
+
+
+
+            quantizer = NewQuantizer(G, reset_all=True)
+            quantizer.options = Opts
+            quantizer.schemes.append('SQ8')
+            quantizer.set_stats(astats)
+            quantizer.quantize()
+            G.add_dimensions()
+            
+            if quant_ne16: # adjust after NE16 = True
+                NNToolShell.run_commands_on_graph(G, ['adjust', 'fusions --scale8'])
+
+            NNToolShell.run_commands_on_graph(G, [ 'qshow'])
+            print("The graph is QUANTIZED")
+
+
+        else: # fp16
+            NNToolShell.run_commands_on_graph(G, [ 
+                'fquant',
+                'qtune --step * scheme=float float_type=float16', 
+                'qshow'])
+
+
+
+        # define the executed
+        executer = GraphExecuter(G, qrecs=G.quantization)
+
+
     for i, file in enumerate(filenames):
 
         # check first if the clean signal has been already produced
@@ -94,29 +174,113 @@ def test_on_gap(    dataset_path, output_file, samplerate, padding,
             estimate, s = librosa.load(estimate_filepath, sr=samplerate)
         else: # compute the estimate
         
-            # Get data
-            if os.path.isfile(output_file):
-                os.remove(output_file)
+            if nntool:
+                # Get data
+                if os.path.isfile(output_file):
+                    os.remove(output_file)
 
-            input_file = noisy_path + file + '.wav'
-            data, s = librosa.load(input_file, sr=samplerate)
-            if padding:
-                data = np.pad(data, (padding, padding))
+                input_file = noisy_path + file + '.wav'    
+    #            input_file = '/home/manuele/GWT_apps/denoiser/samples/quant/p286_035.wav'        
+                data, s = librosa.load(input_file, sr=samplerate)
 
-            file_name =  os.getcwd() + '/samples/test_py.wav'
-            sf.write(file_name, data, samplerate)
+                print(input_file)
 
-            run_on_gap_gvsoc(file_name, output_file, compile=compile_GAP, 
-                gru=gru, quant_bfp16=quant_bfp16, quant_int8=quant_int8, approx=approx)
-            compile_GAP = False
-            shutil.copyfile('BUILD/GAP9_V2/GCC_RISCV_PULPOS/test_gap.wav', output_file)
+                if padding:
+                    data = np.pad(data, (padding, padding))
+                    print(data)
+            
+                win_len = 400
+                win_inc = 100 
+                fft_len = 512
+                print('data input shape:', data.shape)
 
-            if not os.path.isfile(output_file):
-                print("Error! not any output file produced")
-                exit(0)
+                stft_frame_i = librosa.stft(
+                    data, win_length=win_len, 
+                    n_fft=fft_len, hop_length=win_inc,
+                    window='hann', center=False)
+                fft_feat, num_win = stft_frame_i.shape
+                print('stft input shape:', stft_frame_i.shape)
 
-            estimate, s = librosa.load(output_file, sr=samplerate)
-            estimate = estimate[300:]
+
+                stft_frame_i_T = np.transpose (stft_frame_i) # swap the axis to select the tmestamp
+                stft_frame_o_T = np.empty_like(stft_frame_i_T)
+
+                rnn_0_i_state = np.zeros(256)
+                #rnn_0_c_state = np.zeros(256)
+                rnn_1_i_state = np.zeros(256)
+                #rnn_1_c_state = np.zeros(256)
+                
+
+                for i in range (num_win):
+                    print('*****Frame ' + str(i) + ' ******')
+                    stft_clip = stft_frame_i_T[i]
+                    stft_clip_mag = np.abs(stft_clip)
+#                    print(stft_clip_mag)
+                    #stft_clip_mag_estimate = np.random.rand(fft_feat)
+                    data = [stft_clip_mag, rnn_0_i_state, rnn_1_i_state]
+                    outputs = executer.execute(data, 
+                        qmode=QuantizationMode.all_dequantize() if nntool_quantizer else None, 
+                        silent=True)
+                    if nntool_quantizer:
+                        n_id = [4, 32, 36, 40] if quant_ne16 else [4, 31, 34, 39]
+                    else:
+                        n_id = [4, 31, 34, 39]
+                    
+                    conv_0_out = outputs[n_id[0]][0]
+                    rnn_0_i_state = outputs[n_id[1]][0]
+                    rnn_1_i_state = outputs[n_id[2]][0]
+                    mag_out = outputs[n_id[3]][0]
+
+#                    print('conv_0_out= ', conv_0_out.squeeze())
+#                    print('rnn_0_i_state= ', rnn_0_i_state.squeeze())
+#                    print('rnn_1_i_state= ', rnn_1_i_state.squeeze())
+#                    print('mag_out= ', mag_out.squeeze())
+#
+#                ## this is for print values ####
+#                    v = ''
+#                    for item in rnn_1_i_state.squeeze():
+#                        v += str(item) + ', '
+#                    print(v)
+#                ################################
+
+
+
+                    stft_clip_mag_estimate = mag_out.squeeze()
+                    #stft_clip_mag_estimate = np.ones(257)
+
+                    stft_clip = stft_clip * stft_clip_mag_estimate
+                    stft_frame_o_T[i] = stft_clip
+
+#                    if i>-1:
+#                        exit(0)
+
+
+                stft_frame_o = np.transpose (stft_frame_o_T)
+                print('stft output shape:', stft_frame_o.shape)
+
+       
+                data = librosa.istft(stft_frame_o, hop_length=win_inc, 
+                    win_length=win_len, window='hann', center=False )
+                
+                print('data output shape:', data.shape)
+
+                #print(np.mean((outputs1 - np.abs(librosa_stft))**2))
+                
+                estimate = data[padding:] # place holder
+            else:
+
+                input_file = noisy_path + file + '.wav'    
+
+                denoise_sample_on_gap_gvsoc(
+                    input_file, output_file, samplerate, 
+                    padding = padding, compile=compile_GAP, 
+                    gru=gru, quant_bfp16=quant_bfp16, 
+                    quant_int8=quant_int8, approx=approx
+                )
+
+                compile_GAP = False
+                estimate, s = librosa.load(output_file, sr=samplerate)
+                estimate = estimate[300:]
 
             if suffix_cleanfile != '':
                 sf.write(estimate_filepath, estimate, samplerate)
@@ -138,6 +302,7 @@ def test_on_gap(    dataset_path, output_file, samplerate, padding,
         total_cnt += 1
         total_pesq += pesq_i
         total_stoi += stoi_i
+
 
     pesq = total_pesq / total_cnt
     stoi = total_stoi / total_cnt
@@ -195,21 +360,35 @@ if __name__ == "__main__":
                         help="Suffix of the clean smaples in test mode. If empy no clean sample is stored")
     parser.add_argument('--gru', action="store_true",
                             help="Set GRU in case of a GRU model")
-    parser.add_argument('--bfp16', action="store_true",
-                            help="Set quantization to BFP16")
-    parser.add_argument('--int8', action="store_true",
-                            help="Set quantization to 8 bit ") 
+    parser.add_argument("--quant", type=str, default="fp16",
+                        help="fp16 | bfp16 | int8 | ne16")
+#    parser.add_argument('--bfp16', action="store_true",
+#                            help="Set quantization to BFP16")
+#    parser.add_argument('--int8', action="store_true",
+#                            help="Set quantization to 8 bit ") 
+    parser.add_argument('--nntool', action="store_true",
+                            help="Run inference on nntool. if False, run inference on GVSOC")
     parser.add_argument("--approx", type=str, default='',
                         help="Empty | LUT")
 
     args = parser.parse_args()
-    print(args)
+
+    # parse the quantization method
+    bfp16 = int8 = ne16 = False
+    if args.quant == 'bfp16':
+        bfp16 = True
+    elif args.quant == 'int8':
+        int8 = True
+    elif args.quant == 'ne16':
+        ne16 = True
+
+    # call the test
     if args.mode == 'sample':
         print(args.pad_input)
-        denoise_sample(args.wav_input, args.wav_output, args.sample_rate, args.pad_input)
+        denoise_sample_on_gap_gvsoc(args.wav_input, args.wav_output, args.sample_rate, args.pad_input)
     elif args.mode == 'test':
         test_on_gap(args.dataset_path, args.wav_output, args.sample_rate, args.pad_input, 
-            args.suffix_clean, args.gru, args.bfp16, args.int8, args.approx)
+            args.suffix_clean, args.gru, bfp16, int8, ne16, args.nntool, args.approx)
     else:
         print("Selected --mode is not supported!")
         exit(1)
